93f0c3413b7578ef0f0081eb75dd63dec9aefb64
Andraž <123487347+Snicl@users.noreply.github.com>
2023-04-06 09:30:19 +0200

    Update Koda.ino

--- Koda/Koda.ino
+++ Koda/Koda.ino
@@ -1,4 +1,3 @@
-
 #include <Keypad.h>
 #include <Wire.h> 
 #include <LiquidCrystal.h>
@@ -68,16 +67,16 @@ void loop()
       {
           if (first == geslo)
           {
-            lcd.setCursor(0,1);
+            lcd.setCursor(0,0);
             lcd.print(first);
-            delay(200);
+            delay(300);
             lcd.clear();
-            delay(200);
-            //lcd.setCursor(0,1);
+            delay(300);
+            lcd.setCursor(0,0);
             lcd.print(first);
-            delay(200);
+            delay(300);
             lcd.clear();
-            delay(200);
+            delay(300);
             lcd.setCursor(0,3);
             lcd.print("Success");
             digitalWrite(led, HIGH);
